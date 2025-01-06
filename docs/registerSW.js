2ce2f62fa1df85cb8003261350642cc2edc2b45e
EngineerCafeJP <staff@engineercafe.jp>
2025-01-06 19:48:00 +0900

    01061941ChangeDocsToDist

--- docs/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-read-nfc-app/sw.js', { scope: '/react-read-nfc-app/' })})}
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/react-read-nfc-app/' : './',
+  // base: process.env.GITHUB_PAGES ? '/react-read-nfc-app/' : './',
+  base: '/',
   plugins: [
     react(),
     basicSsl(),
@@ -42,6 +43,7 @@ export default defineConfig({
     port: 3000,
   },
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
+    sourcemap: true
   },
 });
